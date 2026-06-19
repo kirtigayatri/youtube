466bcb2a2836449b5f0f1a6df0cb361c615e6a72
kirtigayatri <kirti_ug_23@ee.nits.ac.in>
2026-06-19 23:03:05 +0530

    add user and vedio model

---
+++ src/models/vedio.model.js
@@ -0,0 +1,54 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate
+ from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema =new Schema(
+    {
+          videoFile: {
+            type:string ,//cloudinary url
+            required: true
+
+          },
+          title:{
+            type:string ,//cloudinary url
+            required: true
+
+          },
+          description:{
+            type:string ,//cloudinary url
+            required: true
+
+          },
+          duration:{
+            type:Number,//cloudinary url
+            required: true
+
+          },
+          views:{
+            type:Number,
+            default:0
+          },
+          isPublished:{
+            type:Boolean,
+            default:true
+          },
+          owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+          }
+
+
+
+
+
+
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video =mongoose.model("Vedio",videoSchema)
