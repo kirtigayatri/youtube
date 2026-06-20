c5192b8ffbff6a7d596c9f2a647d8296b37a40c3
kirtigayatri <kirti_ug_23@ee.nits.ac.in>
2026-06-20 19:48:50 +0530

    cloudinary and multer changes

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import{v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+    
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: CLOUDINARY_API_KEY, 
+        api_secret: CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+   const uploadOnCloudinary =async (localFilesPath) => {
+    try{
+        if(!localFilesPath) return null 
+       const response = await cloudinary.uploader.upload(localFilesPath,{
+            resource_type :"auto"
+        })
+
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    } catch(error){
+        fs.unlinkSync(localFilesPath)
+        return null;
+
+    }
+   }
+
+
+
+       
+
+    cloudinary.v2.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function(error, result) {
+    console.log(result);
+  }
+);
