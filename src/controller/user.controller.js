8b2ecbaa094872e487e1dfdde3c47ec99bb7697d
kirtigayatri <kirti_ug_23@ee.nits.ac.in>
2026-06-21 22:51:34 +0530

    add register controller and bug fixes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,11 +1,89 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import{ApiError} from "../utils/ApiError.js"
+import{User} from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 const registerUser =asyncHandler( async (req,res)=>{
-     return res.status(200).json({
-        message: "ok"
+
+    const {fullname,email,username,paaword}=req.body
+    console.log("email:",email);
+
+    if(
+        [fullname,email,username,paaword].some((feild)=>
+        feild?.trim()==="")
+        
+        
+        
+    
+    ){
+       
+      throw new ApiError(400, "All feilds are required")
+
+    }
+
+   const existedUser= User.findOne({
+        $or :[{username},{email}]
     })
+    if(existedUser){
+        throw new ApiError(409,"User with email or username already exists")
+    }
+
+   const avatarLocalPath= req.files?.avatar[0]?.path;
+    const coverImagePath= req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required")
+    }
+     const avatar = await  uploadOnCloudinary(avatarLocalPath)
+      const coverImage= await uploadOnCloudinary(coverImagePath)
+
+
+      if(!avatar){
+        throw new ApiError(400,"Avatar file is required")
+      }
+      const user=User.create({
+        fullNme,
+        avatar :avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+      })
+
+       const createdUser =await User.findById(user._id).select(
+        "-password -refreshToken"
+       )
+
+       if(createdUser){
+
+        throw new ApiError(500,"something went wrong whileregistring user")
+       }
+
+       return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered Successfully")
+       )
+    
+
+
+
+
 })
+//get user detail from frontend 
+//validation kahi empty tho nahi bhej diya
+//check if user already exsist-sabse easy email se check kar lo ya user name se
+//check for images
+//check for avatar
+//upload them to cloudinary,avatar
+//create user object -create entry in db
+//remove password and refresh token feild from response 
+//check for user creation
+//return res 
+
+
+
+
 
 
 
