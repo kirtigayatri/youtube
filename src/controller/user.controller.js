9fc519f996749bf2daf6958ee476c4485be1b4b9
kirtigayatri <kirti_ug_23@ee.nits.ac.in>
2026-06-23 19:09:43 +0530

    controller changes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,7 +4,23 @@ import{User} from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessRefreshTokens=async(userId) =>{
+  try{
+    const user =await User.findById(userId)
+    const accessToken =user.generateAccessToken()
+    const refreshToken =user.generateRefreshToken()
 
+
+    user.refreshToken =refreshToken
+       await user.save({validateBeforeSave: false})
+
+       return {accessToken,refreshToken}
+
+
+  }catch(error){
+   throw new ApiError(500,"something went wrong")
+  }
+}
 const registerUser =asyncHandler( async (req,res)=>{
 
     const {fullname,email,username,paaword}=req.body
@@ -86,7 +102,63 @@ const registerUser =asyncHandler( async (req,res)=>{
 //remove password and refresh token feild from response 
 //check for user creation
 //return res 
+const loginUser  =asyncHandler (async (req,res)=>{
+  // req bodu->data
+  //username
+  //find the user 
+  //password check
+  //access and refresh token
+  //send cookies
 
+  const{email,username,password}= req.body
+  if(!username|| !email){
+    throw new ApiError(400,"username or email  is required")
+  }
+  const user  =await  User.findOne({
+   $or:[{username},{email}]
+
+
+   })
+   if(!user){
+    throw new ApiError(400,"user does not exsist")
+   }
+    
+    const isPasswordValid =await user.isPasswordCorrect(password)
+     if(!isPasswordValid){
+    throw new ApiError(400,"invalid user credentials")
+
+   }
+     const {accessToken,refreshToken} = await  generateAccessRefreshTokens(user._id)
+
+     const loggedInUser =await User.findById(user._id).select("-password -refreshToken")
+
+     const options= {
+      httpOnly : true,
+      secure :true
+
+     }
+     return res
+     .status(200)
+     .cookie("access Token ",accessToken,options)
+     .cookie("refreshToken",refreshToken,options)
+     .json(
+      new ApiResponse(
+        200,
+        {
+
+        user:loggedInUser,AccessToken,
+        refreshToken
+
+        }
+      )
+
+
+
+     )
+
+
+
+})
 
 
 
